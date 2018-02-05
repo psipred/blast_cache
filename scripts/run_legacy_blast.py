13fba17b7fb09ac463b4d516e192b94c4b5917bc
Daneil Buchan <clone@myself.com>
2018-02-05 16:43:06 +0000

    enable db push

--- scripts/run_legacy_blast.py
+++ scripts/run_legacy_blast.py
@@ -116,8 +116,8 @@ if r.status_code == 404 and "No Record Available" in r.text:
                   "data": str(request_data).replace('"', '\\"').replace('\n', '\\n'),
                   }
     # print(entry_data['data'])
-    # r = requests.post(entry_uri, data=entry_data)
-    # print("Submission Response:", r.status_code)
+    r = requests.post(entry_uri, data=entry_data)
+    print("Submission Response:", r.status_code)
 else:
     # get blast file from cache
     print("Cache Response:", r.status_code, "retrieved file from cache")
