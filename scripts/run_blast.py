d09a8efcca79f96ff1301b3bd4720f078b67124d
Daneil Buchan <clone@myself.com>
2018-02-08 10:10:28 +0000

    ensure outputs of script are rw by all

--- scripts/run_blast.py
+++ scripts/run_blast.py
@@ -105,7 +105,7 @@ if r.status_code == 404 and "No Record Available" in r.text:
                          stderr=subprocess.PIPE)
     p.wait()
     end_time = time.time()
-    os.fchmod(out_dir+"/"+seq_name+"."+output_type, 0o666)
+    os.chmod(out_dir+"/"+seq_name+"."+output_type, 0o666)
     runtime = math.ceil(end_time-start_time)
     hit_count = get_num_alignments(out_dir+"/"+seq_name+".xml")
     pssm_data = get_pssm_data(out_dir+"/"+seq_name+"."+output_type)
