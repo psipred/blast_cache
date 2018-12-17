fe5242316e58f9d9b9144f24561a11c084095f1d
Daniel Buchan <clone@myself.com>
2018-12-17 21:10:58 +0000

    fixing blast db building

--- scripts/run_hhblits_legacy_blast.py
+++ scripts/run_hhblits_legacy_blast.py
@@ -179,8 +179,9 @@ if r.status_code == 404 and "No Record Available" in r.text:
         out_dir+"/"+seq_name+".flat " + \
         "-t "+out_dir+"/"+seq_name+".flat"
     blast_cmd = blast_bin+"blastpgp -d "+out_dir+"/" + \
-        seq_name+".flat -i " + \
-        blast_input+" -B "+out_dir+"/"+seq_name+".psi -C " + \
+        seq_name+".flat -i "+blast_input + \
+        #" -B "+out_dir+"/"+seq_name+".psi -C " + \
+        " -C " + \
         out_dir+"/"+seq_name+".chk -a 2 "+blast_settings+" -m 7 -o " + \
         out_dir+"/"+seq_name+".xml "
 
