c443c52fb89ca54344891cae096360090df187c3
Daniel Buchan <clone@myself.com>
2019-03-04 19:21:47 +0000

    making single fasta for legacy blast

--- scripts/run_legacy_blast.py
+++ scripts/run_legacy_blast.py
@@ -106,7 +106,6 @@ request_data = dict(zip(i, i))
 
 r = requests.get(entry_query, params=request_data)
 print("Cache Response:", r.status_code)
-exit()
 if r.status_code == 404 and "No Record Available" in r.text:
     print("Running blast")
     cmd = ''
