5d4f1ed5264d2eb6e8fd3501093af656dbcae1e4
Daniel Buchan <clone@myself.com>
2020-06-03 16:39:48 +0100

    update webhooks

--- scripts/run_hhblits_legacy_blast.py
+++ scripts/run_hhblits_legacy_blast.py
@@ -211,7 +211,7 @@ upload = True
 if r.status_code == 500:
     upload = False
     print("Seq MD5: "+file_contents['md5'])
-    alert_r = requests.post('https://hooks.slack.com/services/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', json={"text": ":rage:\n500 response from BLAST CACHE\nSeq MD5: "+file_contents['md5']})
+    alert_r = requests.post('https://hooks.slack.com/services/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', json={"text": ":rage:\n500 response from BLAST CACHE\nSeq MD5: "+file_contents['md5']})
     print("Slack Response:", alert_r.status_code, alert_r.text)
 # if r.status_code == 404 and ("No Record Available" in r.text or
 #                              "No Entries Available" in r.text or
